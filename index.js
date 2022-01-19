d42cd64bc88a4217a972d6eb06cfbd46a74a05da
jccho96 <joanne.yy.cho@gmail.com>
2022-01-19 07:11:40 -0800

    play sound

--- index.js
+++ index.js
@@ -2,7 +2,43 @@ let numberOfDrumBoxes = document.querySelectorAll("button").length;
 
 for (i = 0; i < numberOfDrumBoxes; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function () {
-    let sound = new Audio("sounds/tom-1.mp3");
-    sound.play();
+    let buttonInnerHTML = this.innerHTML;
+
+    switch (buttonInnerHTML) {
+      case "w":
+        let tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+
+      case "a":
+        let tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "s":
+        let tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "d":
+        let tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      case "j":
+        let snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+      case "k":
+        let crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+      case "l":
+        let kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+
+      default:
+        console.log(buttonInnerHTML);
+    }
   });
 }
+
+//
