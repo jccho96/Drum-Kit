09e991233aab789e33a3d6102a65d9b5583d4e60
jccho96 <joanne.yy.cho@gmail.com>
2022-01-19 06:30:20 -0800

    add background image to drums

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let numberOfDrumBoxes = document.querySelectorAll("button").length;
 
 for (i = 0; i < numberOfDrumBoxes; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function () {
-    alert("I am clicked");
+    let sound = new Audio("sounds/tom-1.mp3");
+    sound.play();
   });
 }
