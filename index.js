4fc7a43c53c027b38aa907b93cd4a1c98fd2b4d4
jccho96 <joanne.yy.cho@gmail.com>
2022-01-19 05:52:02 -0800

    addEventListener to boxes

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-alert("Hello");
+let numberOfDrumBoxes = document.querySelectorAll("button").length;
+
+for (i = 0; i < numberOfDrumBoxes; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    alert("I am clicked");
+  });
+}
